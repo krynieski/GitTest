0d44d38375f18ab6890557c9d67df9b0ed27d05b
kryyhu <kryyhoo@gmail.com>
2017-04-04 21:50:03 +0200

    develop: print implementation added

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "../include/print.h"
 
 int main()
 {
-        printf("Hello World\n");
+        print("Hello World");
         return 0;
 }
---
+++ source/print.c
@@ -0,0 +1,7 @@
+#include "../include/print.h"
+#include <stdio.h>
+
+void print(char* string)
+{
+        printf("%s\n", string);
+}
